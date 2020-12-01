09f1bd82dfcfe3e8062cf8ba1aa583b772757308
Joonas Mattila <153542@sskky.fi>
2020-12-01 12:25:06 +0200

    asiakas=kayttaja

--- asiakas.php
+++ asiakas.php
@@ -21,6 +21,7 @@
                     <table class="table table-striped table-bordered">
                         <thead>
                             <tr>
+                                <th>ID</th>
                                 <th>Nimi</th>
                                 <th>Sähköposti</th>
                                 <th>Puhelinnumero</th>
@@ -32,18 +33,19 @@
                     <?php
                             include 'database.php';
                             $pdo = Database::connect();
-                            $sql = 'SELECT *, CONCAT(etunimi, " ", sukunimi) nimi FROM asiakas';
+                            $sql = 'SELECT *, CONCAT(etunimi, " ", sukunimi) nimi FROM kayttaja';
                             foreach ($pdo->query($sql) as $row) {
                                 echo '<tr>';
+                                echo '<td>'. $row['kayttajaID'] . '</td>';
                                 echo '<td>'. $row['nimi'] . '</td>';
                                 echo '<td>'. $row['sahkoposti'] . '</td>';
-                                echo '<td>'. $row['puhelin'] . '</td>';
+                                echo '<td>'. $row['puhelinnumero'] . '</td>';
                                 echo '<td>'. $row['postitoimipaikka'] . '</td>';echo '<td width=250>';
-                                echo '<a class="btn btn-info" href="read.php?id='.$row['asiakasID'].'">Tarkista</a>';
+                                echo '<a class="btn btn-info" href="read.php?id='.$row['kayttajaID'].'">Tarkista</a>';
                                 echo ' ';
-                                echo '<a class="btn btn-success" href="paivita_asiakas.php?id='.$row['asiakasID'].'">Päivitä</a>';
+                                echo '<a class="btn btn-success" href="paivita_asiakas.php?id='.$row['kayttajaID'].'">Päivitä</a>';
                                 echo ' ';
-                                echo '<a class="btn btn-danger" href="poista_asiakas.php?id='.$row['asiakasID'].'">Poista</a>';
+                                echo '<a class="btn btn-danger" href="poista_asiakas.php?id='.$row['kayttajaID'].'">Poista</a>';
                                 echo '</td>';
                                 echo '</tr>';
                             }
