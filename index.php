d03e2f4e4ca83c1af205a2d268ccd3a3d4491fcd
kalamakala <153540@sskky.fi>
2020-12-02 13:07:17 +0200

    index pof
    ag

--- index.php
+++ index.php
@@ -53,44 +53,51 @@
   <div class="album py-5 bg-light">
     <div class="container">
 
-    <?php
-     include 'database.php';
-    $pdo = Database::connect();
-    $sql = 'SELECT * FROM tuote WHERE tuotenimi LIKE "%' . $hakusana . '%"';
-    foreach ($pdo->query($sql) as $row) {
-                            
-      ?>
 
-      <div class="row">
-      <div class="col-md-4">
-        <div class="card mb-4 shadow-sm">
-              <?php
-                    echo '<td>'. $row['tuotenimi'] . '</td>';
-                    echo '<td>'. "<img src='img/{$row['kuva']}' width='300' height='300'>" . '</td>';
-                    echo '<td>'. $row['lisatiedot'] . '</td>';
-              ?>
-                <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-              </div>
-              
-              <?php
-                  echo '<td>'. $row['hinta'] . '</td>';
-              ?>
-                  <small class="text-muted">
-              <?php
-                  echo '<td>'. $row['tuotenimi'] . '</td>';
-              ?>
-              </small>
-            </div>
-          </div>
+    <div class="container">
+        <div class="row">
+            <h1 style="font-size:500    %;font-family:helvetica;font-weight:bold;">MYYNNISSÄ</h1>
         </div>
-      </div>
+            <div class="row">
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                        <tr>
+                                <th>Kuva</th>
+                                <th>Nimi/Hinta</th>
+                                <th>Lisätiedot</th>
+                                <th>OSTA</th>
+                        </tr>
+                        </thead>
+                  <tbody>
+                    <?php
+                            include 'database.php';
+                            $pdo = Database::connect();
+                            $sql = 'SELECT * FROM tuote';
+                            foreach ($pdo->query($sql) as $row) {
+                                echo '<tr>';
+                                echo '<td>'. "<img src='img/{$row['kuva']}' width='100' height='100'>" . '</td>';
+                                echo '<td>';
+                                echo '<h3>'. $row['tuotenimi'] .'</h3>';
+                                echo '<h6>'. $row['hinta'] . "€" . '</h6>';
+                                echo '</td>';
+                                echo '<td>'. $row['lisatiedot'] . '</td>';
+                                echo '<td>';
+                                echo '<a class="btn btn-info" href="katso_tuote.php?id='.$row['tuoteID'].'">Katso lisää</a>';
+                                echo '</td>';
+                                 echo '</td>';
+                                 echo '</tr>';
+                            }
+                            Database::disconnect();
+                        ?>
+                  </tbody>
+            </table>
+        </div>
+    </div> <!-- /container -->
     
-      <?php
-    }
+     </body>
+</html>
     
-                            ?>
+                        
                             
 
   
